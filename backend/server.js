aba45376fe2eeaa6701ca843dd19a558ea2b2e9f
Usha Kumari <uavaswani@gmail.com>
2024-03-14 09:54:30 +0100

    changes back to local host

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 // Add your CORS middleware
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://ushas-projects-d69436a1.vercel.app');
   // Other CORS headers if needed
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
